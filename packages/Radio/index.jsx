1e30f1cf7870d846c0048d2b54f6bd02ff7e2025
Mias <812137533@qq.com>
2021-03-08 21:41:04 +0800

    fix: fix radio event unwork

--- packages/Radio/index.jsx
+++ packages/Radio/index.jsx
@@ -6,9 +6,22 @@ const { normalSizes } = theme
 const [createComponent] = createNameSpace('Radio')
 import './radio.less'
 
+
+const queryRadioSize = (radioSize)=>{
+  const size = {
+    mini:'12px',
+    small:'14px',
+    medium:'16px',
+    large:'18px',
+  }
+  return size[radioSize]
+}
+
+
 export default createComponent({
   props: {
     checked: Boolean,
+    disabled:Boolean,
     value: { type: String, default: '11' },
     size: {
       type: String,
@@ -18,27 +31,46 @@ export default createComponent({
   },
   emtis: ['change'],
   setup(props, { attrs, slots, emit }) {
-    const { checked, value, size } = toRefs(props)
+    const { checked, value, size ,disabled } = toRefs(props)
+    const isDisabled = ref(disabled.value)
     const selfChecked = ref(!!checked.value)
-    console.log(selfChecked)
+    
     const handlerChange = (e) => {
-      emit('change', e)
+      if (isDisabled.value) return
+      const radioEvent = {
+        target:{
+          checked:!selfChecked.value,
+        },
+        stopPropagation: e.stopPropagation,
+        preventDefault: e.preventDefault,
+        nativeEvent: e,
+      }
+      selfChecked.value = !selfChecked.value;
+      emit('change', radioEvent)
     }
 
-    //size =>12px 14px 16px 18px
+    const calcRadioSize = computed(()=>{
+      const _size =  queryRadioSize(size.value)
+      const style = {}
+      style['--radioSize'] = _size
+      return style
+    })
 
     return () => (
-      <div className={`fay-radio ${attrs?.class ? attrs.class : ''}`}>
-        <label>
+      <div className={`fay-radio ${attrs?.class ? attrs.class : ''}`} style={calcRadioSize.value}>
+        <label className={` ${isDisabled.value && 'disabled'}`}>
           <input
             {...attrs}
             type="radio"
+            value={value.value}
             checked={selfChecked.value}
             onChange={handlerChange}
+            disabled={isDisabled.value}
           ></input>
-          <span className={'.fay-radio-name'}>
+          <span className={'fay-radio-name'}>
+            <span 
+              className={`fay-radio-point ${isDisabled.value && 'disabled'} ${selfChecked.value && 'active'}`}  />
             {slots.default?.()}
-            <span className={'.fay-radio-point'}></span>
           </span>
         
         </label>
