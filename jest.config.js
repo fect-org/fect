a097e0a073673def8993f2432fc3da1dc03574a3
Mias <812137533@qq.com>
2021-03-10 22:15:55 +0800

    test(avatar): add ava test case

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  testPathIgnorePatterns: ['/node_modules/', '/dist/', '/lib/', '/src/'],
+  collectCoverageFrom: [
+    'packages/**/*.{js,jsx,vue}',
+    '!packages/utils/*',
+    '!packages/index.js',
+  ],
   preset: '@vue/cli-plugin-unit-jest',
   transform: {
     '^.+\\.vue$': 'vue-jest',
