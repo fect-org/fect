4e1e1a7f3041db7dbbe3933c3374380e74f4c5ab
Mias <812137533@qq.com>
2021-03-15 16:26:04 +0800

    chore: add colllect componentRouter script

---
+++ scripts/collectPage.js
@@ -0,0 +1,62 @@
+const fs = require('fs-extra')
+const extractMetadata = require('extract-mdx-metadata')
+const path = require('path')
+const docsPath = path.join(__dirname, '../docs/zh-cn/components')
+const routerPath = path.join(__dirname, '../src/router/index.js')
+const testPath = path.join(__dirname, '../test.js')
+
+const getMetaData = async (files, parent_path) => {
+  return Promise.all(
+    files.map(async (file) => {
+      const filePath = path.join(parent_path, file)
+      const content = await fs.readFile(filePath, 'utf-8')
+      const meta = await extractMetadata(content)
+      const componentName = file.replace('.mdx', '')
+      const url = filePath
+        .replace(docsPath, '')
+        .replace('.mdx', '')
+        .replace(/\\/g, '/')
+      return {
+        name: meta.title || file,
+        componentName,
+        url,
+        group: meta.group || null,
+      }
+    }),
+  )
+}
+
+const routerTempalte = (routes, routeFile) => {
+  const routerPool = []
+  const _templte = [
+    'import { createRouter, createWebHistory } from \'vue-router\';\n',
+  ]
+  routes.map((route, i) => {
+    // console.log(route)
+    routerPool.push({
+      path: route.url,
+      name: route.componentName,
+      component: route.componentName,
+    })
+    // console.log(routerPool[i])
+    _templte.push(
+      `import ${route.componentName} from '../../docs/zh-cn/components/${route.componentName}.mdx';\n`,
+    )
+  })
+  var reTemp = `${_templte.join('')}\n const routes=[${JSON.stringify(
+    routerPool,
+  )}];\n const router = createRouter({
+    history: createWebHistory(process.env.BASE_URL),
+    routes,
+  });\n export default router;`
+  // console.log(reTemp)
+  return reTemp
+}
+
+;(async () => {
+  const docFiles = await fs.readdir(docsPath)
+  const res = await getMetaData(docFiles, docsPath)
+  const tes = await routerTempalte(res)
+  // console.log(tes)
+  await fs.writeFile(testPath, tes)
+})()
