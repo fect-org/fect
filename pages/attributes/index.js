fa9eaa457657c0e4bfee0cd72a9ae89156f013c7
Mias <812137533@qq.com>
2021-04-09 13:15:31 +0800

    docs: remove pages/components and update all docs style

--- pages/attributes/index.js
+++
@@ -1,6 +0,0 @@
-import Attributes from './attributes'
-import AttributesTitle from './attributes-title'
-
-Attributes.title = AttributesTitle
-
-export default Attributes
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,1 @@
-export { default as Layout } from './layout'
 export { default as CodeShow } from './codeShow'
-export { default as Attributes } from './attributes'
