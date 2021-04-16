bd9b1ca681c3c2e42b77a16f775a6d809daf1195
XeryYue <812137533@qq.com>
2021-04-16 23:11:47 +0800

    fix: adjust anchor work

--- _page/Anchor/index.jsx
+++ _page/Anchor/index.jsx
@@ -1,4 +1,4 @@
-import { defineComponent, ref } from 'vue'
+import { defineComponent, onMounted, ref } from 'vue'
 import AnchorIcon from './anchor.icon'
 
 import './anchor.common.less'
@@ -9,23 +9,19 @@ const Anchor = defineComponent({
     const tar = ref('')
     const anchorRef = ref(null)
 
+    onMounted(() => {
+      const el = anchorRef.value.innerText
+      tar.value = anchorEncode(el)
+    })
+
     const anchorEncode = (text) => {
       if (!text) return undefined
       return text.toLowerCase().replace(/ /g, '')
     }
 
-    const handlerClickAnchor = () => {
-      const el = anchorRef.value.innerText
-      tar.value = anchorEncode(el)
-    }
-
     return () => (
       <>
-        <span
-          className="f_doc-anchor"
-          ref={anchorRef}
-          onClick={handlerClickAnchor}
-        >
+        <span className="f_doc-anchor" ref={anchorRef}>
           {/* style={{ fontSize: '1.65rem' }} */}
           <fectLink href={`#${tar.value}`}>{slots.default?.()}</fectLink>
           <span className="f_doc-anchor_virtual" id={tar.value}></span>
