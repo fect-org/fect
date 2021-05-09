50f9dc092d85b3d06f829588a04984aab5642f81
XeryYue <812137533@qq.com>
2021-05-09 22:03:25 +0800

    docs(image): add image docs

--- docs/example/image/skeleton.vue
+++ docs/example/image/skeleton.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-    <fe-image :src="img" width="150px" height="150px"></fe-image>
+    <fe-image :src="img" width="150px" height="150px" skeleton></fe-image>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'ex-image-default',
+  name: 'ex-image-skeleton',
   setup() {
     const img = 'https://avatars.githubusercontent.com/u/52351095?v=4'
     return { img }
