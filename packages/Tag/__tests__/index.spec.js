e8c533ff67099c001cd2d9dd6ea3ca56a929156c
XeryYue <812137533@qq.com>
2021-05-19 18:00:29 +0800

    docs(tag): add tag docs

--- packages/Tag/__tests__/index.spec.js
+++ packages/Tag/__tests__/index.spec.js
@@ -11,4 +11,42 @@ describe('Tag', () => {
     expect(wrapper.html()).toMatchSnapshot()
     expect(() => wrapper.unmount()).not.toThrow()
   })
+  it('should support different type', () => {
+    const wrapper = mount({
+      setup() {
+        return {
+          types: ['default', 'success', 'warning', 'error'],
+        }
+      },
+      render() {
+        return (
+          <>
+            {this.types.map((type, i) => (
+              <Tag text={i} type={type} key={type} />
+            ))}
+          </>
+        )
+      },
+    })
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+  it('should support invert ', () => {
+    const wrapper = mount({
+      setup() {
+        return {
+          types: ['default', 'success', 'warning', 'error'],
+        }
+      },
+      render() {
+        return (
+          <>
+            {this.types.map((type, i) => (
+              <Tag text={i} type={type} key={type} useInvert />
+            ))}
+          </>
+        )
+      },
+    })
+    expect(wrapper.html()).toMatchSnapshot()
+  })
 })
