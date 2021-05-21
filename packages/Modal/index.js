94635ba74ad2aa91636542ae3608908f19613f2a
XeryYue <812137533@qq.com>
2021-05-21 21:06:42 +0800

    chore(remove): remove modal serve.js

--- packages/Modal/index.js
+++ packages/Modal/index.js
@@ -1,9 +1,7 @@
 import FectModal from './modal'
-import Modal from './serve'
 
 Modal.install = (vue) => {
   vue.use(FectModal)
-  vue.config.globalProperties.$modal = Modal
 }
 
 export default Modal
