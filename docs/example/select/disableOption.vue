e9a07bc1e887da7170d08f4251ec691e5c5d399c
XeryYue <812137533@qq.com>
2021-05-27 22:21:14 +0800

    docs(select): add unit test and docs

--- docs/example/select/disableOption.vue
+++ docs/example/select/disableOption.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <fe-select placeholder="placeholder" v-model="val">
-      <fe-option label="Vue" value="0"></fe-option>
+      <fe-option label="Vue" value="0" disabled></fe-option>
       <fe-option label="React" value="1"></fe-option>
     </fe-select>
   </div>
@@ -10,7 +10,7 @@
 <script>
 import { ref } from 'vue'
 export default {
-  name: 'ex-select-default',
+  name: 'ex-select-disableOption',
   setup() {
     const val = ref(null)
     return { val }
--- docs/example/select/disabled.vue
+++ docs/example/select/disabled.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <fe-select placeholder="placeholder" v-model="val">
+    <fe-select placeholder="placeholder" v-model="val" disabled>
       <fe-option label="Vue" value="0"></fe-option>
       <fe-option label="React" value="1"></fe-option>
     </fe-select>
@@ -10,7 +10,7 @@
 <script>
 import { ref } from 'vue'
 export default {
-  name: 'ex-select-default',
+  name: 'ex-select-disabled',
   setup() {
     const val = ref(null)
     return { val }
