e2bad67ff1a965d443c624af6b2bbfb780f5679c
XeryYue <812137533@qq.com>
2021-05-31 20:29:03 +0800

    build(babel): add process jug

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,11 @@
+const presets = () => {
+  if (process.env.NODE_ENV === 'production') {
+    return '@babel/preset-env'
+  }
+  return '@vue/cli-plugin-babel/preset'
+}
+
 module.exports = {
-  presets: ['@babel/preset-env'],
+  presets: [presets()],
   plugins: ['@vue/babel-plugin-jsx'],
 }
