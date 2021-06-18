b5b76c1ad232a9858c983579c255e5dcfae3f328
XeryYue <812137533@qq.com>
2021-06-19 00:30:54 +0800

    docs(progress): add docs and vetur

---
+++ docs/example/progress/colors.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <fe-button @click="handleClick" auto>增加</fe-button>
+    <fe-spacer inline />
+    <fe-button @click="() => (val = 10)" auto>重置</fe-button>
+    <fe-spacer :y="0.5" />
+    <fe-progress :value="val" :max="100" :colors="colors" />
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+  name: 'ex-progress-colors',
+  setup() {
+    const colors = {
+      20: 'red',
+      40: 'blue',
+      60: 'green',
+      80: '#000',
+      100: 'pink',
+    }
+    const val = ref(10)
+
+    const handleClick = () => (val.value += 10)
+    return {
+      colors,
+      val,
+      handleClick,
+    }
+  },
+}
+</script>
---
+++ docs/example/progress/types.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <fe-progress :value="val" :max="100" />
+    <fe-spacer :y="0.5" />
+    <fe-progress :value="val" :max="100" type="success" />
+    <fe-spacer :y="0.5" />
+    <fe-progress :value="val" :max="100" type="warning" />
+    <fe-spacer :y="0.5" />
+    <fe-progress :value="val" :max="100" type="error" />
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+  name: 'ex-progress-types',
+  setup() {
+    const val = ref(10)
+
+    return {
+      val,
+    }
+  },
+}
+</script>
