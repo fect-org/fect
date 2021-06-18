6b490a76b543d156477c1ef4b8c7b1e0bfa6bc4b
XeryYue <812137533@qq.com>
2021-06-19 00:30:54 +0800

    docs(progress): add docs and vetur

---
+++ docs/example/progress/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <fe-progress :value="val" :max="100" />
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+  name: 'ex-progress-default',
+  setup() {
+    const val = ref(10)
+
+    return {
+      val,
+    }
+  },
+}
+</script>
