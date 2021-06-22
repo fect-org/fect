39979d491b5446b708449521fc4528eda3cab24a
XeryYue <812137533@qq.com>
2021-06-22 17:51:55 +0800

    docs(image): add docs and vetur helper

--- docs/example/image/invert.vue
+++ docs/example/image/invert.vue
@@ -1,12 +1,21 @@
 <template>
   <div>
-    <fe-image width="150px" height="150px" :src="img"></fe-image>
+    <fe-image
+      :src="img"
+      width="540px"
+      height="246px"
+      useBrowser
+      invert
+      target="__blank"
+      url="https://vue.miaya.com"
+      skeleton
+    ></fe-image>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'ex-image-default',
+  name: 'ex-image-invert',
   setup() {
     const img
       = 'https://user-images.githubusercontent.com/52351095/118687359-7e809480-b837-11eb-8083-b0504ec79652.png'
--- docs/example/image/usebrowser.vue
+++ docs/example/image/usebrowser.vue
@@ -1,12 +1,19 @@
 <template>
   <div>
-    <fe-image width="150px" height="150px" :src="img"></fe-image>
+    <fe-image
+      :src="img"
+      width="540px"
+      height="246px"
+      useBrowser
+      target="__blank"
+      url="https://vue.miaya.com"
+    ></fe-image>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'ex-image-default',
+  name: 'ex-image-browser',
   setup() {
     const img
       = 'https://user-images.githubusercontent.com/52351095/118687359-7e809480-b837-11eb-8083-b0504ec79652.png'
