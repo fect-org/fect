9fb7e275af28f2cc554276e9cd245464cdb75cc3
XeryYue <812137533@qq.com>
2021-07-02 14:34:44 +0800

    docs(skeleton): add vetur helper & component docs

---
+++ docs/example/skeleton/more.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <fe-button @click="handleChange">重新加载</fe-button>
+    <fe-skeleton :loading="load">
+      <template #skeleton>
+        <div
+          style="display: inline-flex;width:100%;"
+          v-for="(_, i) in 3"
+          :key="i"
+        >
+          <fe-skeletonItem variable="circle" style="margin-bottom:10px;" />
+          <div style="display:flex;flex-direction: column;">
+            <fe-skeletonItem
+              variable="h3"
+              style="margin-left: var(--fay-gap-half); width:60px;"
+            />
+            <fe-skeletonItem
+              style="margin-left: var(--fay-gap-half); width:60px;"
+            />
+          </div>
+        </div>
+      </template>
+      <div v-for="(_, i) in 3" :key="i" style="margin-top:10px;">
+        <User />
+      </div>
+    </fe-skeleton>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+import User from './user.vue'
+export default {
+  name: 'ex-skeleton-more',
+  components: {
+    User,
+  },
+  setup() {
+    const load = ref(false)
+    const handleChange = () => {
+      load.value = false
+      setTimeout(() => (load.value = true), 2000)
+    }
+    return {
+      load,
+      handleChange,
+    }
+  },
+}
+</script>
