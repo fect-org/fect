96b9583cf9f258f36cb5c0e27dc9a7ec8007f0a4
XeryYue <812137533@qq.com>
2021-07-31 17:30:51 +0800

    docs(rating): complete docs and vetur

---
+++ docs/example/rating/custom.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <fe-rating v-model="val" :icon="setIcon()" locked @change="handleChange" />
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+  name: 'ex-rating-custom',
+  setup() {
+    const val = ref(3)
+    const setIcon = () => <github />
+    return { val, setIcon }
+  },
+}
+</script>
