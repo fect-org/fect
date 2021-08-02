8aadb000fa06cd59de55589d3d1ed7b6dcc01c42
XeryYue <812137533@qq.com>
2021-08-03 00:43:26 +0800

    test(unit): update unit test case

--- packages/link/__tests__/index.spec.tsx
+++ packages/link/__tests__/index.spec.tsx
@@ -1,14 +1,14 @@
-import { mount } from '@vue/test-utils'
 import Link from '../index'
+import { mount } from '@vue/test-utils'
 
 describe('Link', () => {
-  it('should be render as element', () => {
-    const link = mount(Link)
-    expect(link.html()).toMatchSnapshot()
-    expect(() => link.unmount()).not.toThrow()
+  it('should be render as a element', () => {
+    const wrapper = mount(Link)
+    expect(wrapper.html()).toMatchSnapshot()
+    expect(() => wrapper.unmount()).not.toThrow()
   })
 
-  it('should be render correctly', () => {
+  it('should be render correctly', async () => {
     const wrapper = mount({
       render() {
         return (
@@ -30,6 +30,18 @@ describe('Link', () => {
         )
       },
     })
+
     expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  it('props to should higher than props href', async () => {
+    const wrapper = mount(Link, {
+      props: {
+        to: '#',
+      },
+    })
+    const el = wrapper.find('.fect-link')
+    await el.trigger('click')
+    expect(() => wrapper.unmount()).not.toThrow()
   })
 })
