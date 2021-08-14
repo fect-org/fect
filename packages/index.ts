8a1e5dd8a5da53e31f0e1a0f339c263d448496b4
XeryYue <812137533@qq.com>
2021-08-14 14:11:30 +0800

    refactor(radio): complete refacotr and update unit test case

--- packages/index.ts
+++ packages/index.ts
@@ -1,49 +1,49 @@
-import { App } from 'vue'
-import './utils/styles/index.css'
-import Avatar from './avatar'
-import AvatarGroup from './avatar-group'
-import Badge from './badge'
-import BadgeAnchor from './badge-anchor'
-import Breadcrumbs from './breadcrumbs'
-import BreadcrumbsItem from './breadcrumbs-item'
-import Button from './button'
-import Capacity from './capacity'
-import Card from './card'
-import Checkbox from './checkbox'
-import CheckboxGroup from './checkbox-group'
-import Code from './code'
-import Col from './col'
-import Collapse from './collapse'
-import CollapseGroup from './collapse-group'
-import Dot from './dot'
-import Grid from './grid'
-import Image from './image'
-import Input from './input'
-import Link from './link'
-import Loading from './loading'
-import Modal from './modal'
-import Pagination from './pagination'
-import Progress from './progress'
-import Radio from './radio'
-import RadioGroup from './radio-group'
-import Rating from './rating'
-import Row from './row'
-import Select from './select'
-import SelectOption from './select-option'
-import Skeleton from './skeleton'
-import SkeletonItem from './skeleton-item'
-import Snippet from './snippet'
-import Spacer from './spacer'
-import Swipe from './swipe'
-import SwipeItem from './swipe-item'
-import Switch from './switch'
-import Tab from './tab'
-import Tabs from './tabs'
-import Tag from './tag'
-import Teleport from './teleport'
-import Toast from './toast'
-const components = [
-  Avatar,
+
+import { App } from 'vue';
+import './utils/styles/index.css';
+import Avatar from './avatar';
+import AvatarGroup from './avatar-group';
+import Badge from './badge';
+import BadgeAnchor from './badge-anchor';
+import Breadcrumbs from './breadcrumbs';
+import BreadcrumbsItem from './breadcrumbs-item';
+import Button from './button';
+import Capacity from './capacity';
+import Card from './card';
+import Checkbox from './checkbox';
+import CheckboxGroup from './checkbox-group';
+import Code from './code';
+import Col from './col';
+import Collapse from './collapse';
+import CollapseGroup from './collapse-group';
+import Dot from './dot';
+import Grid from './grid';
+import Image from './image';
+import Input from './input';
+import Link from './link';
+import Loading from './loading';
+import Modal from './modal';
+import Pagination from './pagination';
+import Progress from './progress';
+import Radio from './radio';
+import RadioGroup from './radio-group';
+import Rating from './rating';
+import Row from './row';
+import Select from './select';
+import SelectOption from './select-option';
+import Skeleton from './skeleton';
+import SkeletonItem from './skeleton-item';
+import Snippet from './snippet';
+import Spacer from './spacer';
+import Swipe from './swipe';
+import SwipeItem from './swipe-item';
+import Switch from './switch';
+import Tab from './tab';
+import Tabs from './tabs';
+import Tag from './tag';
+import Teleport from './teleport';
+import Toast from './toast';
+const components = [Avatar,
   AvatarGroup,
   Badge,
   BadgeAnchor,
@@ -84,17 +84,16 @@ const components = [
   Tabs,
   Tag,
   Teleport,
-  Toast,
-]
-const install = (app: App) => {
-  components.map((component: any) => {
+  Toast];
+const install = (app:App) => {
+  components.map((component:any) => {
     if (component.install) {
       app.use(component)
     } else if (component.name) {
-      app.component(component.name, component)
+      app.component(component.name , component)
     }
   })
-}
+};
 
 export {
   install,
@@ -140,8 +139,10 @@ export {
   Tag,
   Teleport,
   Toast,
-}
+};
 
 export default {
   install,
-}
+};
+
+  
---
+++ packages/radio-group/__tests__/index.spec.ts
@@ -0,0 +1,102 @@
+import { mount } from '@vue/test-utils'
+import Radio from '../../radio'
+import RadioGroup from '../index'
+
+describe('Radio', () => {
+  it('should be render as a element', () => {
+    const wrapper = mount(Radio)
+    expect(() => wrapper.unmount()).not.toThrow()
+  })
+  it('should be support modelVaue to control initial checked', () => {
+    const wrapper = mount({
+      components: {
+        [Radio.name]: Radio,
+        [RadioGroup.name]: RadioGroup,
+      },
+      data() {
+        return { val: '1' }
+      },
+      template: `
+      <div class="container">
+        <fe-radioGroup v-model="val">
+          <fe-radio value="1">Test</fe-radio>
+          <fe-radio value="2">Test1</fe-radio>
+        </fe-radioGroup> 
+      </div>
+      `,
+    })
+    const els = wrapper.findAll('.fect-radio__point')
+    expect(els[0].classes('active')).toBe(true)
+  })
+
+  it('should be support noraml props like size , disabled , useRow', () => {
+    const wrapper = mount({
+      components: {
+        [Radio.name]: Radio,
+        [RadioGroup.name]: RadioGroup,
+      },
+      data() {
+        return { size: 'mini', useRow: true, disabled: true }
+      },
+      template: `
+      <div class="container">
+        <fe-radioGroup :size="size" :useRow="useRow" :disabled="disabled">
+          <fe-radio value="1">Test</fe-radio>
+          <fe-radio value="2">Test1</fe-radio>
+        </fe-radioGroup> 
+      </div>
+      `,
+    })
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  it('should be emit update:checked when only use radio and unset disabled', async () => {
+    const wrapper = mount(Radio, {
+      props: {
+        value: '1',
+        checked: false,
+        disabled: false,
+      },
+    })
+    const el = wrapper.find('input')
+    await el.trigger('change')
+    expect(wrapper.emitted('update:checked')![0]).toEqual([true])
+  })
+
+  it('should emit update:modelValue in radio group', async () => {
+    const wrapper = mount({
+      components: {
+        [Radio.name]: Radio,
+        [RadioGroup.name]: RadioGroup,
+      },
+      data() {
+        return { val: '1' }
+      },
+      template: `
+      <div class="container">
+        <fe-radioGroup v-model="val">
+          <fe-radio value="1">Test</fe-radio>
+          <fe-radio value="2">Test1</fe-radio>
+        </fe-radioGroup> 
+      </div>
+      `,
+    })
+    const el = wrapper.findAll('input')
+    const els = wrapper.findAll('.fect-radio__point')
+    await el[1].trigger('change')
+    expect(els[1].classes('active')).toBe(true)
+  })
+
+  it('should be disabled all event when set props disabled', async () => {
+    const wrapper = mount(Radio, {
+      props: {
+        value: 1,
+        disabled: true,
+      },
+    })
+    const el = wrapper.find('input')
+    await el.trigger('change')
+    expect(wrapper.emitted('change')).not.toBeTruthy()
+    expect(wrapper.emitted('change')).toBeFalsy()
+  })
+})
