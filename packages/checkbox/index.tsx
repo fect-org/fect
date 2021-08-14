fd5ff8c7402257de6e370c00a5599b135366b9b4
XeryYue <812137533@qq.com>
2021-08-14 21:40:37 +0800

    refactor(checbox): complete refacotr

--- packages/checkbox/index.tsx
+++ packages/checkbox/index.tsx
@@ -1,6 +1,6 @@
-import { computed, ref, watchEffect, PropType } from 'vue'
+import { computed, watchEffect, PropType, watch } from 'vue'
 import { useProvider } from '@fect-ui/vue-hooks'
-import { createNameSpace } from '../utils'
+import { createNameSpace, useState } from '../utils'
 import { NormalSizes } from '../utils/theme/propTypes'
 import { CustomCSSProperties } from '../utils/base'
 import {
@@ -32,94 +32,82 @@ export default createComponent({
       default: 'medium',
     },
     label: {
-      type: [String],
+      type: String,
       default: '',
     },
   },
   emits: ['change', 'update:modelValue'],
   setup(props, { slots, emit }) {
-    const isChecked = ref<boolean>(props.modelValue)
-    const selfSize = ref<NormalSizes>(props.size)
-    const isDisabled = ref<boolean>(props.disabled)
+    const [selfChecked, setSelfChecked] = useState<boolean>(props.modelValue)
     const { context } = useProvider<CheckboxGroupProvide>(READONLY_CHECKBOX_KEY)
 
+    const selfSize = computed(() => {
+      if (context) return context.props.size
+      return props.size
+    })
+
+    const selfDisabled = computed(() => {
+      if (context) return context.props.disabled
+      return props.disabled
+    })
+
     const setCurrentState = () => {
-      const { modelValue } = context!.props
-      const value = modelValue.slice()
-      isChecked.value = Boolean(value.find((v) => v === props.label))
+      const parent = context!.parentValue.value
+      const checked = parent.some((v) => v === props.label)
+      setSelfChecked(checked)
     }
 
-    const setCurrentStyle = () => {
-      isDisabled.value = context!.props.disabled
-      selfSize.value = context!.props.size
-    }
-
-    const setParentValue = (event: CheckboxEvent) => {
-      const { label } = props
-      const { modelValue } = context!.props
-      const { updateParentValue, handlerParentChange } = context!
-      const value = modelValue.slice()
-      if (!isChecked.value) {
-        const index = value.indexOf(label)
-        value.splice(index, 1)
-      } else {
-        if (value.indexOf(label) === -1) {
-          value.push(label)
-        }
-      }
-      updateParentValue(value)
-      handlerParentChange({ ...event, target: { value } })
+    if (context) {
+      watchEffect(setCurrentState)
     }
 
     const handleChange = (e: Event) => {
-      isChecked.value = !isChecked.value
-      const CheckboxEvent: CheckboxEvent = {
-        target: {
-          checked: isChecked.value,
-        },
+      if (selfDisabled.value) return
+      setSelfChecked(!selfChecked.value)
+      const checkboxEvent: CheckboxEvent = {
+        target: {},
         stopPropagation: e.stopPropagation,
         preventDefault: e.preventDefault,
         nativeEvent: e,
       }
-      if (context) {
-        setParentValue(CheckboxEvent)
-      }
-      if (!context) {
-        emit('update:modelValue', isChecked.value)
-        emit('change', CheckboxEvent)
-      }
-    }
 
-    if (context) {
-      watchEffect(() => {
-        setCurrentState()
-        setCurrentStyle()
+      if (context) {
+        context.updateParentValue(props.label, selfChecked.value)
+        context.parentChangeHandler(checkboxEvent)
+        return
+      }
+
+      emit('change', {
+        ...checkboxEvent,
+        target: { checked: selfChecked.value },
       })
     }
 
+    watch(selfChecked, (cur) => emit('update:modelValue', cur))
+
     const setCheckBoxBaseSize = computed(() => {
-      const _size = queryCheckboxSize(selfSize.value)
+      const size = queryCheckboxSize(selfSize.value)
       const style: CustomCSSProperties = {}
-      style['--checkboxSize'] = _size
+      style['--checkboxSize'] = size
       return style
     })
 
     return () => (
       <label
-        class={`fect-checkbox ${isDisabled.value ? 'disabled' : ''}`}
+        class={`fect-checkbox ${selfDisabled.value ? 'disabled' : ''}`}
         style={setCheckBoxBaseSize.value}
       >
         <CheckIcon
-          class={`${isDisabled.value ? 'disabled' : ''}`}
-          checked={isChecked.value}
+          class={`${selfDisabled.value ? 'disabled' : ''}`}
+          checked={selfChecked.value}
         />
         <input
           type="checkbox"
-          disabled={isDisabled.value}
-          checked={isChecked.value}
+          disabled={selfDisabled.value}
+          checked={selfChecked.value}
           onChange={handleChange}
         ></input>
-        <span class="fect-text_inner">{slots.default?.()}</span>
+        <span class="fect-checkbox__inner">{slots.default?.()}</span>
       </label>
     )
   },
