d35e758e2c652c8b93757baae40988e669a7d1f2
XeryYue <812137533@qq.com>
2021-08-24 18:45:44 +0800

    perf(script): remove reducer.js and perf collectRouter code

--- scripts/collectRouter.js
+++ scripts/collectRouter.js
@@ -1,10 +1,12 @@
 const collectRoute = (context) => {
   return context.keys().map((p) => {
-    const sourceName = p.match(/\w+(?=.md)/g)
-    // eslint-disable-next-line prefer-destructuring
-    const name = sourceName[0]
-    const nameUper = name.charAt(0).toUpperCase() + name.slice(1)
-    return { path: `/${name}`, name: nameUper, component: context(p).default }
+    const sourceName = p.match(/\w+(?=.md)/g).toString()
+    const routeName = sourceName.charAt(0).toUpperCase() + sourceName.slice(1)
+    return {
+      path: `/${routeName}`,
+      name: routeName,
+      component: context(p).default,
+    }
   })
 }
 
