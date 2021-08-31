d40fb57e2adb8440d28a9d385adbf44925275355
lvh666 <lvhao4748@163.com>
2021-08-31 22:54:55 +0800

    test(breadcrumbs): update breadcrumbs unit test case with ts
    test(breadcrumbs): update breadcrumbs unit test case with ts
    
    BREAKING CHANGE: update breadcrumbs unit test case with ts

--- packages/breadcrumbs/__tests__/index.spec.ts
+++ packages/breadcrumbs/__tests__/index.spec.ts
@@ -2,29 +2,45 @@ import { mount } from '@vue/test-utils'
 import Breadcrumbs from '../index'
 import BreadcrumbsItem from '../../breadcrumbs-item'
 
+const BreadcrumbsTest = {
+  template: '<Breadcrumbs />',
+}
+
+const DiffWrapper = {
+  components: {
+    [Breadcrumbs.name]: Breadcrumbs,
+    [BreadcrumbsItem.name]: BreadcrumbsItem,
+  },
+  data () {
+    return {
+      sizes: ['mini', 'small', 'medium', 'large'],
+    }
+  },
+  template: `
+    <fe-breadcrumbs v-for="(size, idx) in sizes" :size="size" :key="idx">
+      {idx}
+    </fe-breadcrumbs>`,
+}
+
+const Wrapper = {
+  components: {
+    [Breadcrumbs.name]: Breadcrumbs,
+    [BreadcrumbsItem.name]: BreadcrumbsItem,
+  },
+  template: `<fe-breadcrumbs separator=">">
+    <fe-breadcrumbsItem />
+    <fe-breadcrumbsItem />
+  </fe-breadcrumbs>`,
+}
+
 describe('Breadcrumbs', () => {
   it('should be render as element', () => {
-    const braedcrumbs = mount(<Breadcrumbs />)
+    const braedcrumbs = mount(BreadcrumbsTest)
     expect(() => braedcrumbs.unmount()).not.toThrow()
   })
 
   it('should be support different sizes', () => {
-    const wrapper = mount({
-      setup() {
-        return { sizes: ['mini', 'small', 'medium', 'large'] }
-      },
-      render() {
-        return (
-          <>
-            {this.sizes.map((size, idx) => (
-              <Breadcrumbs size={size} key={idx}>
-                {idx}
-              </Breadcrumbs>
-            ))}
-          </>
-        )
-      },
-    })
+    const wrapper = mount(DiffWrapper)
 
     const els = wrapper.findAll('.fect-breadcrumbs')
     expect(els[0].attributes('style')).toBe('font-size: 12px;')
@@ -35,12 +51,7 @@ describe('Breadcrumbs', () => {
   })
 
   it('should be support custom separator', () => {
-    const wrapper = mount(
-      <Breadcrumbs separator=">">
-        <BreadcrumbsItem />
-        <BreadcrumbsItem />
-      </Breadcrumbs>,
-    )
+    const wrapper = mount(Wrapper)
     expect(wrapper.find('.fect-breadcrumbs__separator').text()).toBe('>')
   })
 })
