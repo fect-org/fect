ee2b0cf203d0974f2a051f57a4b3e20eddbc8cbe
XeryYue <812137533@qq.com>
2021-08-31 18:20:37 +0800

    test(badge): update badge unit test case

---
+++ packages/badge/__tests__/index.spec.ts
@@ -0,0 +1,38 @@
+import { mount } from '@vue/test-utils'
+import Badge from '..'
+
+const Wrapper = {
+  components:{
+    [Badge.name]:Badge,
+  },
+  data(){
+    return {
+      type:'default',
+      dot:false,
+      size:'medium',
+    }
+  },
+  template:`<div class="container">
+   <fe-badge :type="type" :dot="dot" :size="size" :type-data="type">Badge</fe-badge>
+ </div>`, 
+}
+
+
+describe('Badge',()=>{
+  it('should be render as a element',()=>{
+    const badge = mount(Badge)
+    expect(()=>badge.unmount()).not.toThrow()
+  })
+  it('should be supprot noramlize props',async()=>{
+    const wrapper = mount(Wrapper)
+    const el = wrapper.find('.fect-badge')
+    await wrapper.setData({ type:'success' })
+    expect(el.attributes('type-data')).toBe('success')
+    await wrapper.setData({ size:'large' })
+    expect(el.attributes('style')).toBe('font-size: 16px;')
+    await wrapper.setData({ dot:true })
+    expect(wrapper.find('.fect-dot')).toBeTruthy()
+    expect(wrapper.find('.fect-dot').text()).toBe('')
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+})
