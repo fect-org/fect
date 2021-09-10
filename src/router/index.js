2f920a105e0670cf52c57ff06a42951900ce2d5d
XeryYue <812137533@qq.com>
2021-09-10 11:16:33 +0800

    chore(scripts): remove scripts and move path

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import { collectRoute } from '../../scripts/collectRouter'
-// const path = require.context('../../docs/zh-cn/', true, /.md$/)
 const path = require.context('../../docs/zh-cn', true, /.md$/)
 
-collectRoute(path)
+const collectRoute = (context) => {
+  return context.keys().map((p) => {
+    const sourceName = p.match(/\w+(?=.md)/g).toString()
+    const routeName = sourceName.charAt(0).toUpperCase() + sourceName.slice(1)
+    return {
+      path: `/${routeName}`,
+      name: routeName,
+      component: context(p).default,
+    }
+  })
+}
 
 const routes = [
   { path: '/', redirect: { name: 'Introduce' } },
