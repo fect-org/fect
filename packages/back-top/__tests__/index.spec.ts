f4a6c0effd48c47fa4a1757127534eee24954b16
lwindh <lvhao4748@163.com>
2021-09-26 12:44:07 +0800

    refactor(back-top): change backTop test

--- packages/back-top/__tests__/index.spec.ts
+++ packages/back-top/__tests__/index.spec.ts
@@ -1,7 +1,8 @@
 import BackTop from '..'
-import { mount } from '@vue/test-utils'
+import { mount, flushPromises } from '@vue/test-utils'
+import { later } from '../../../tests'
 
-const Wrapper = {
+const CustomWrapper = {
   components:{
     [BackTop.name]:BackTop,
   },
@@ -14,7 +15,27 @@ const Wrapper = {
   },
   template:`<div class="container" ref="scrollTarget" style="height: 100px; overflow: auto">
     <div style="height: 10000px; width: 100%;">
-      <fe-back-top :right="right" :bottom="bottom" :visibilityHeight="visibilityHeight" >
+      <fe-back-top ref="target" :right="right" :bottom="bottom" :visibilityHeight="visibilityHeight" >
+        Custom Context
+      </fe-back-top>
+    </div>
+  </div>`,
+}
+
+const Wrapper = {
+  components:{
+    [BackTop.name]:BackTop,
+  },
+  data(){
+    return {
+      duration: 1,
+      right: 50,
+      bottom: 50,
+    }
+  },
+  template:`<div class="container" ref="scrollTarget" style="height: 100px; overflow: auto">
+    <div style="height: 10000px; width: 100%;">
+      <fe-back-top ref="target" :target="() => $refs.scrollTarget" :duration="duration" :right="right" :bottom="bottom" :visibilityHeight="visibilityHeight" >
         Custom Context
       </fe-back-top>
     </div>
@@ -31,8 +52,14 @@ describe('BackTop', () => {
     expect(wrapper.html()).toMatchSnapshot()
     expect(() => wrapper.unmount()).not.toThrow()
   })
-  it('should support props right and bottom',async () => {
-    const wrapper = mount(Wrapper)
+  it('should support props right and bottom', () => {
+    const wrapper = mount(BackTop, { 
+      props: {
+        right: 50,
+        bottom: 50,
+        visibilityHeight: 10,
+      },
+    })
     const el = wrapper.find('.fect-back-top')
     expect(el.attributes('style')).toBe(
       'right: 50px; bottom: 50px;',
@@ -40,15 +67,33 @@ describe('BackTop', () => {
     expect(wrapper.html()).toMatchSnapshot()
   })
   it('should be support custom slots',async () => {
+    const wrapper = mount(CustomWrapper)
+    const el = wrapper.find('.fect-back-top')
+    expect(el.text()).toBe('Custom Context')
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+  it('should be support scroll visible',async () => {
     const wrapper = mount(Wrapper)
-    // const el = wrapper.find('.fect-back-top')
-    // expect(el.text()).toBe('')
-    // const refs = wrapper.vm.$refs as any
-    // refs.scrollTarget.scrollTop = 2000
-    // await new Promise(resolve => setTimeout(() => resolve(1), 50))
-    // expect(el.text()).toBe('Custom Context')
-    // await el.trigger('click')
-    // expect(el.text()).toBe('')
+    const el = wrapper.find('.fect-back-top')
+    expect(el.text()).toBe('')
+
+    const refs = wrapper.vm.$refs as any
+    const {
+      target: { handleScroll },
+    } = refs
+    refs.scrollTarget.scrollTop = 2000
+    await handleScroll({ target: refs.scrollTarget })
+    await flushPromises()
+    await later()
+    expect(el.text()).toBe('Custom Context')
+
+    await el.trigger('click')
+    await flushPromises()
+    await later()
+    await handleScroll({ target: refs.scrollTarget })
+    await flushPromises()
+    await later()
+    expect(el.text()).toBe('')
     expect(wrapper.html()).toMatchSnapshot()
   })
 })
