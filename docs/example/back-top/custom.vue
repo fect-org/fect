7aae1c95124afd597c48b3e5689c00731a282e2d
XeryYue <812137533@qq.com>
2021-09-26 19:21:08 +0800

    fix(back-top): fix in mobile back-top abnormal behavior & perf logoic

--- docs/example/back-top/custom.vue
+++ docs/example/back-top/custom.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <fe-back-top :bottom="100">
+    <fe-back-top :bottom="100" :target="fn" visibilityHeight="10">
       <div class="fect-back-top-custom">UP</div>
     </fe-back-top>
     Scroll down to see the bottom-right button.
@@ -10,18 +10,22 @@
 <script>
 export default {
   name: 'ex-back-top-custom',
+  setup() {
+    const fn = () => document.querySelector('.fect-doc__main')
+    return { fn }
+  },
 }
 </script>
 
 <style lang="less" scoped>
-  .fect-back-top-custom {
-    height: 40px;
-    width: 40px;
-    line-height: 40px;
-    border-radius: 4px;
-    background-color: #1088e9;
-    color: #fff;
-    text-align: center;
-    font-size: 14px;
-  }
+.fect-back-top-custom {
+  height: 40px;
+  width: 40px;
+  line-height: 40px;
+  border-radius: 4px;
+  background-color: #1088e9;
+  color: #fff;
+  text-align: center;
+  font-size: 14px;
+}
 </style>
