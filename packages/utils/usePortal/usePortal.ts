28075384e57739744fc0cede47ec317fbfba19a0
kanno <812137533@qq.com>
2021-10-06 14:32:41 +0800

    refactor(toast): refactor toast component (#117)

---
+++ packages/utils/usePortal/usePortal.ts
@@ -0,0 +1,20 @@
+import { createApp, Component, unref } from 'vue'
+import { ElementRef } from '../useRect'
+
+const createNode = (name: string) => {
+  const dom = document.querySelector(`.${name}`)
+  if (dom) return dom
+  const elSnapshot = document.createElement('div')
+  elSnapshot.classList.add(name)
+  document.body.appendChild(elSnapshot)
+  return elSnapshot
+}
+
+const createPortal = (children: Component, container?: ElementRef) => {
+  const elSnapshot = unref(container) || document.createElement('div')
+  const app = createApp(children)
+  document.body.appendChild(elSnapshot)
+  app.mount(elSnapshot)
+}
+
+export { createNode, createPortal }
