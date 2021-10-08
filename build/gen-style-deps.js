4a3729025154ed2fa6ab6fd0612a296b095863c1
XeryYue <812137533@qq.com>
2021-10-08 20:33:07 +0800

    fix(build): fix in build style analyze lose component

--- build/gen-style-deps.js
+++ build/gen-style-deps.js
@@ -1,16 +1,15 @@
-const { outputFileSync, readFile, remove } = require('fs-extra')
+const { outputFileSync, remove, readJSON } = require('fs-extra')
 const { join, dirname } = require('path')
 
 const compilerStyleDeps = async (file) => {
-  const data = await readFile(file, 'utf8')
-  const deps = JSON.parse(JSON.parse(data)).style
-  const depsKeys = Object.keys(deps)
+  const data = await readJSON(file, 'utf8')
+  const depsKeys = Object.keys(data)
   const { BABEL_ENV } = process.env
 
   const styles = depsKeys
     .map((key) => {
-      if (BABEL_ENV === 'commonjs') return `require("${deps[key]}");\n`
-      return `import "${deps[key]}";\n`
+      if (BABEL_ENV === 'commonjs') return `require("${data[key]}");\n`
+      return `import "${data[key]}";\n`
     })
     .join('')
   const output = join(dirname(file), 'style', 'index.js')
