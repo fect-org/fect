8a9d0360b2baf40d732d3da24f5738582f61dfa6
XeryYue <812137533@qq.com>
2021-10-12 18:17:53 +0800

    chore: update jest conf

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,12 @@
 module.exports = {
   testPathIgnorePatterns: ['/node_modules/', '/dist/', '/lib/', '/src/'],
   collectCoverageFrom: [
-    'packages/vue/**/*.{js,jsx,ts,tsx,vue}',
-    '!packages/vue/utils/**/*',
-    '!packages/index.ts',
-    '!packages/vue/**/type.ts',
-    '!packages/vue/**/style.ts',
+    'packages/vue/components/**/*.{js,jsx,ts,tsx,vue}',
+    '!packages/vue/test/**/*',
+    '!packages/vue/components/utils/**',
+    '!packages/vue/components/index.ts',
+    '!packages/vue/components/**/type.ts',
+    '!packages/vue/components/**/style.ts',
   ],
   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
   testEnvironment: 'jest-environment-jsdom-sixteen',
