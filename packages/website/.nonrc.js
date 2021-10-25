c3311b08854f37fe8c0045411107e050c2ac518f
XeryYue <812137533@qq.com>
2021-10-25 21:52:02 +0800

    chore(config): website add icon and add build command
    affects: @fect-ui/website

--- packages/website/.nonrc.js
+++ packages/website/.nonrc.js
@@ -2,6 +2,6 @@ const { join } = require('path')
 const Markdown = require('vite-plugin-md')
 
 module.exports = {
-  entry: join(__dirname, 'src'),
+  entry: join(__dirname, 'index.html'),
   plugins: [Markdown],
 }
