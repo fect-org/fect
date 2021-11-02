0132da657c1908888d902e213042b4934d3ea50b
深淵の鴿子 <71170059+ZerxZ@users.noreply.github.com>
2021-11-02 01:43:05 +0100

    feat(md-plugin):fix syntax

--- packages/md-loader/index.js
+++ packages/md-loader/index.js
@@ -5,85 +5,90 @@
  * vite plugin details see : https://cn.vitejs.dev/guide/api-plugin.html#universal-hooks
  */
 
-const MarkdownIt = require('markdown-it')
+ const MarkdownIt = require('markdown-it')
 
-class PluginParser {
-  constructor(options) {
-    this.options = options
-    this.name = 'vite-md-plugin'
-    this.enforce = 'pre'
-  }
-  /**
-   *
-   * @param {string} file
-   * @returns {boolean}
-   */
-  hasMarkdown(file) {
-    const reg = /.mdx?$/g
-    return reg.test(file)
-  }
-
-  resolveMarkdownOptions() {
-    this.options = Object.assign(
-      {
-        markdownClasses: 'fect-md__wrapper',
-        mardownWrapper: 'section',
-        markdownOptions: {},
-      },
-      this.options
-    )
-  }
-  /**
-   *
-   * @param {string} raw
-   * @param {MarkdownIt} createMarkdown
-   */
-  markdwonParser(raw, createMarkdown) {
-    return createMarkdown.render(raw)
-  }
-
-  /**
-   *
-   * @param {string} raw
-   * @param {MarkdownIt} createMarkdown
-   */
-  parserToVue(raw, createMarkdown) {
-    const templateStr = this.markdwonParser(raw, createMarkdown)
-    const { markdownClasses: classes, mardownWrapper: wrapper } = this.options
-
-    return `<template>
-       <${wrapper} class="${classes}">
-         ${templateStr}
-       </${wrapper}>
-    </template>`
-  }
-  parser() {
-    this.resolveMarkdownOptions()
-    const md = new MarkdownIt({ html: true, linkify: true, typographer: true, ...this.options.markdownOptions })
-    const that = this
-
-    return {
-      name: this.name,
-      enforce: this.enforce,
-      //   transform .md file to vue component
-      transform(code, id) {
-        if (!that.hasMarkdown(id)) return
-        try {
-          return that.parserToVue(code, md)
-        } catch (e) {
-          this.error(e)
-        }
-      },
-      //   make it work in hmr
-      handleUpdate: async (ctx) => {
-        if (!this.hasMarkdown(ctx.file)) return
-        const reader = ctx.read
-        ctx.read = async () => this.parserToVue(ctx.file, await reader())
-      },
-    }
-  }
-}
-
-const markdownPlugin = (options) => new PluginParser(options).parser()
-
-module.exports = { markdownPlugin }
+ class PluginParser {
+   constructor(options) {
+     this.options = this.resolveMarkdownOptions(options)
+     this.name = 'vite-md-plugin'
+     this.enforce = 'pre'
+   }
+   static create(options) {
+     if (!PluginParser.Parser) {
+       PluginParser.Parser = new PluginParser(options)
+     }
+     return PluginParser.Parser.parser()
+   }
+   /**
+    *
+    * @param {string} file
+    * @returns {boolean}
+    */
+   static hasMarkdown(file) {
+     const reg = /.mdx?$/g
+     return reg.test(file)
+   }
+ 
+   resolveMarkdownOptions(options) {
+     return Object.assign(
+       {
+         markdownClasses: 'fect-md__wrapper',
+         markdownWrapper: 'section',
+         markdownOptions: {},
+       },
+       options
+     )
+   }
+   /**
+    *
+    * @param {string} raw
+    * @param {MarkdownIt} createMarkdown
+    */
+   markdownParser(raw, createMarkdown) {
+     return createMarkdown.render(raw)
+   }
+ 
+   /**
+    *
+    * @param {string} raw
+    * @param {MarkdownIt} createMarkdown
+    */
+   parserToVue(raw, createMarkdown) {
+     const templateStr = this.markdownParser(raw, createMarkdown)
+     const { markdownClasses: classes, markdownWrapper: wrapper } = this.options
+ 
+     return `<template>
+        <${wrapper} class="${classes}">
+          ${templateStr}
+        </${wrapper}>
+     </template>`
+   }
+   parser() {
+     const md = new MarkdownIt({ html: true, linkify: true, typographer: true, ...this.options.markdownOptions })
+     const { hasMarkdown, Parser } = PluginParser
+     return {
+       name: this.name,
+       enforce: this.enforce,
+       //   transform .md file to vue component
+       transform(code, id) {
+         if (!hasMarkdown(id)) return
+         try {
+           return Parser.parserToVue(code, md)
+         } catch (e) {
+           this.error(e)
+         }
+       },
+       //   make it work in hmr
+       handleUpdate: async (ctx) => {
+         if (!hasMarkdown(ctx.file)) return
+         const reader = ctx.read
+         ctx.read = async () => Parser.parserToVue(ctx.file, await reader())
+       },
+     }
+   }
+ }
+ 
+ const markdownPlugin = (options) => PluginParser.create(options)
+ 
+ module.exports = { markdownPlugin }
+ 
