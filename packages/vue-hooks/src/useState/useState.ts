b167fd020d0dae693ff3652bbf29cb0f4481d687
XeryYue <812137533@qq.com>
2021-12-24 14:45:47 +0800

    feat(vue-hooks): useState dispatch support functional updates
    affects: @fect-ui/vue-hooks

--- packages/vue-hooks/src/useState/useState.ts
+++ packages/vue-hooks/src/useState/useState.ts
@@ -1,10 +1,16 @@
-import { ref, readonly, UnwrapRef, Ref } from 'vue'
+import { ref, readonly } from 'vue'
+import type { UnwrapRef, Ref } from 'vue'
 
-export type Dispatch<T> = (val: T) => void
+export type SetStateAction<S> = S | ((prevState: S) => S)
+
+export type Dispatch<T> = (val: SetStateAction<T>) => void
 
 const useState = <T>(initial?: T) => {
   const state = ref<T | undefined>(initial)
-  const dispatch = (next: T) => (state.value = next as UnwrapRef<T>)
+  const dispatch = (next: SetStateAction<T>) => {
+    const draft = typeof next === 'function' ? (next as (prevState: T) => T)(state.value as T) : next
+    state.value = draft as UnwrapRef<T>
+  }
   return [readonly(state), dispatch] as [Ref<T>, Dispatch<T>]
 }
 
