8e06cfe9cba2f884eb32f3c9f196e93560eb4f33
kanno <812137533@qq.com>
2021-12-30 23:04:15 +0800

    feat(composables): use-theme support work in ssr
    affects: @fect-ui/vue

--- packages/vue/components/utils/composables/use-theme.ts
+++ packages/vue/components/utils/composables/use-theme.ts
@@ -1,28 +1,33 @@
 /**
- * provide fect-ui default theme system .
+ * provide fect-ui default theme system.
+ * Version:1.1.0  can work in ssr.
  * Author: kanno
  */
 
-import { watch } from 'vue'
-import { isBrowser } from '../format'
+import { watch, onMounted } from 'vue'
 import { useState } from '@fect-ui/vue-hooks'
 import type { Theme } from './interface'
 
 export const useTheme = () => {
-  const [theme, setTheme] = useState<Theme>((localStorage.getItem('theme') as Theme) || 'light-theme')
+  const [theme, setTheme] = useState<Theme>()
 
-  const themeChange = () => setTheme(theme.value === 'light-theme' ? 'dark-theme' : 'light-theme')
+  const themeChange = () => {
+    if (typeof window === 'undefined' || !window.localStorage) return
+    setTheme((pre) => (pre === 'light-theme' ? 'dark-theme' : 'light-theme'))
+  }
 
-  watch(
-    theme,
-    (pre) => {
-      localStorage.setItem('theme', pre)
-      if (!isBrowser()) return
-      const root = document.querySelector('html') as HTMLElement
-      root.setAttribute('class', theme.value)
-    },
-    { immediate: true }
-  )
+  onMounted(() => {
+    if (typeof window === 'undefined' || !window.localStorage) return
+    const theme = (localStorage.getItem('theme') as Theme) || 'light-theme'
+    setTheme(theme)
+  })
+
+  watch(theme, (pre) => {
+    if (typeof window === 'undefined' || !window.localStorage) return
+    localStorage.setItem('theme', pre)
+    const root = document.querySelector('html') as HTMLElement
+    root.setAttribute('class', theme.value)
+  })
 
   return {
     themeChange,
