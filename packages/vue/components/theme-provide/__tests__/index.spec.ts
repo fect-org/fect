11bf4ccac594a562d142063484e389e7f42aa358
kanno <812137533@qq.com>
2022-01-03 21:11:17 +0800

    test(themeProvide): update unit test case
    affects: @fect-ui/vue

--- packages/vue/components/theme-provide/__tests__/index.spec.ts
+++ packages/vue/components/theme-provide/__tests__/index.spec.ts
@@ -13,4 +13,29 @@ describe('ThemeProvide', () => {
     const el = wrapper.find('.fect-theme__context')
     expect(el.attributes('style')).toBe('--primary-foreground: red;')
   })
+  it('when user entry null or undefine should be work correctly', () => {
+    const wrapper = mount(ThemeProvide, {
+      props: {
+        vars: null as any
+      }
+    })
+    const el = wrapper.find('.fect-theme__context')
+    expect(el.attributes('style')).toBe(undefined)
+  })
+  it('when user pass a kebaCase variable should be work', () => {
+    const wrapper = mount(ThemeProvide, {
+      props: {
+        vars: {
+          '--primary-foreground': 'red'
+        }
+      }
+    })
+    const el = wrapper.find('.fect-theme__context')
+    expect(el.attributes('style')).toBe('--primary-foreground: red;')
+  })
+  it('theme-provide will preset a empty object for work', () => {
+    const wrapper = mount(ThemeProvide)
+    const el = wrapper.find('.fect-theme__context')
+    expect(el.attributes('style')).toBe(undefined)
+  })
 })
