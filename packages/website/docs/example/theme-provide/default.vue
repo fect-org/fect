0818e73609a0a27aa8694e1aab7de679b53dd178
XeryYue <812137533@qq.com>
2022-01-04 20:16:22 +0800

    docs(theme): update theme-provide docs

--- packages/website/docs/example/theme-provide/default.vue
+++ packages/website/docs/example/theme-provide/default.vue
@@ -2,7 +2,9 @@
   <fe-theme-provide :vars="cssVars">
     <fe-badge>Example</fe-badge>
     <fe-spacer />
-    <fe-button size="mini" auto @click="setThemeHandler">Theme Change</fe-button>
+    <fe-tag text="Example" />
+    <fe-spacer />
+    <fe-button size="mini" auto :loading="load" @click="setThemeHandler">Theme Change</fe-button>
   </fe-theme-provide>
 </template>
 
@@ -13,15 +15,24 @@ export default {
   name: 'ExThemeProvideDefault',
   setup() {
     const cssVars = ref(null)
-    const setThemeHandler = () =>
-      (cssVars.value = cssVars.value
-        ? null
-        : {
-            primaryForeground: '#0070f3'
-          })
+    const load = ref(false)
+
+    const otherThemes = {
+      badgeDefaultBgColor: '#0070f3',
+      tagDefaultColor: '#0070f3'
+    }
+
+    const setThemeHandler = () => {
+      load.value = !load.value
+      window.setTimeout(() => {
+        cssVars.value = cssVars.value ? null : otherThemes
+        load.value = !load.value
+      }, 2000)
+    }
 
     return {
       cssVars,
+      load,
       setThemeHandler
     }
   }
