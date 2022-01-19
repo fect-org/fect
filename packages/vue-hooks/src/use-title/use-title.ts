fe9923215e70eb2956cbdb99a7c69842e6077d08
XeryYue <812137533@qq.com>
2022-01-19 17:23:05 +0800

    feat(hooks): use-title support dynamic change

--- packages/vue-hooks/src/use-title/use-title.ts
+++ packages/vue-hooks/src/use-title/use-title.ts
@@ -1,11 +1,25 @@
-import { onMounted, onBeforeUnmount, onDeactivated } from 'vue'
+import { onMounted, onBeforeUnmount, onDeactivated, watch } from 'vue'
+import { useState } from '../use-state'
 
-const useTitle = (title: string) => {
-  const setTitle = () => (document.title = title)
-  const resetTitle = () => (document.title = '')
-  onMounted(setTitle)
-  onBeforeUnmount(resetTitle)
-  onDeactivated(resetTitle)
+const useTitle = (initialTitle = '') => {
+  const [title, setTitle] = useState<string>(initialTitle)
+
+  const titleChange = (title = '') => setTitle(title)
+
+  const setDocumentTitle = () => (document.title = title.value)
+  const resetDocumentTitle = () => (document.title = '')
+
+  onMounted(setDocumentTitle)
+
+  watch(title, setDocumentTitle)
+
+  onBeforeUnmount(resetDocumentTitle)
+  onDeactivated(resetDocumentTitle)
+
+  return {
+    titleChange,
+    title
+  }
 }
 
 export { useTitle }
