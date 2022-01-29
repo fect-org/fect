d6a7dfac8a9c2fcee9e7151f3bae56b732c9b877
kanno <812137533@qq.com>
2022-01-29 20:13:06 +0800

    feat(cli): use postcss transform css

--- packages/cli/src/node/compiler/compile-style.ts
+++ packages/cli/src/node/compiler/compile-style.ts
@@ -4,8 +4,12 @@
 
 import fs from 'fs'
 import { render } from 'less'
+import postcss from 'postcss'
 import { replaceExt } from '../../shared/constant'
 import { logErr } from '../../shared/logger'
+
+import CleanCss from 'clean-css'
+const cleanCss = new CleanCss()
 
 export const resolveStyleFile = async (filePath: string) => {
   try {
@@ -14,12 +18,23 @@ export const resolveStyleFile = async (filePath: string) => {
     if (filePath.endsWith('.less')) {
       styleRaw = await fs.promises.readFile(filePath, 'utf-8')
       const { code } = await transformLess(styleRaw, filePath)
+      const style = await transofrmCss(code)
       resolvePath = replaceExt(filePath, '.css')
-      fs.writeFileSync(resolvePath, code)
+      fs.writeFileSync(resolvePath, style)
       fs.unlinkSync(filePath)
     }
   } catch (error) {
     logErr(`Compile style failed at: ${filePath}`)
+    throw error
+  }
+}
+
+const transofrmCss = async (styleRaw: string) => {
+  try {
+    const { css } = await postcss([require('autoprefixer')]).process(styleRaw, { from: undefined })
+    return cleanCss.minify(css).styles
+  } catch (error) {
+    logErr(error)
     throw error
   }
 }
