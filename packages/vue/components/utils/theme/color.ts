2f13720263c761b2ba07645c55cb8787347e7110
kanno <812137533@qq.com>
2022-01-30 22:56:16 +0800

    fix: incoming color not expected

--- packages/vue/components/utils/theme/color.ts
+++ packages/vue/components/utils/theme/color.ts
@@ -1,4 +1,5 @@
-const hexToRgb = (hex: string): number[] => {
+const hexColorToRgb = (hex: string): number[] => {
+  hex = hex.indexOf('#') === 0 ? hex.slice(1) : hex
   const bigint = parseInt(hex, 16)
   const r = (bigint >> 16) & 255
   const g = (bigint >> 8) & 255
@@ -7,9 +8,9 @@ const hexToRgb = (hex: string): number[] => {
 }
 
 const addColorAlpha = (hex: string, alpha = 1) => {
-  const [r, g, b] = hexToRgb(hex)
+  const [r, g, b] = hexColorToRgb(hex)
   const safeAlpha = alpha > 1 ? 1 : alpha < 0 ? 0 : alpha
   return `rgba(${r}, ${g}, ${b}, ${safeAlpha})`
 }
 
-export { hexToRgb, addColorAlpha }
+export { hexColorToRgb, addColorAlpha }
