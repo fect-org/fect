fc6b0e8588cbcffca79ae41780b658797eb515e3
kanno <812137533@qq.com>
2022-02-08 22:29:51 +0800

    docs:split web docs chunk

--- packages/website/.nonrc.ts
+++ packages/website/.nonrc.ts
@@ -1,6 +1,15 @@
 import { markdownPlugin } from '@fect-ui/md-loader'
 import { defineNonConfig } from '@fect-ui/cli'
 import Prism from 'prismjs'
+import { dependencies } from './package.json'
+
+const renderChunks = (deps: Record<string, string>) => {
+  const chunks = {}
+  Object.keys(deps).forEach((key) => {
+    chunks[key] = [key]
+  })
+  return chunks
+}
 
 export default defineNonConfig({
   plugins: [
@@ -9,5 +18,16 @@ export default defineNonConfig({
         highlight: (str) => Prism.highlight(str, Prism.languages.javascript, 'javascript')
       }
     })
-  ]
+  ],
+  viteConfigure: {
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            ...renderChunks(dependencies)
+          }
+        }
+      }
+    }
+  }
 })
