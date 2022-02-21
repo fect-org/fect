bf2a007c7e96b43eaba7180a140c237c2c6b9a1e
kanno <812137533@qq.com>
2022-02-21 19:39:58 +0800

    feat: add hooks useKeyboard

--- packages/vue/components/utils/format/window.ts
+++ packages/vue/components/utils/format/window.ts
@@ -1,1 +1,6 @@
 export const isBrowser = () => Boolean(typeof window !== null && window !== undefined && window.document)
+
+export const isMac = () => {
+  if (!isBrowser()) return false
+  return navigator.platform.toUpperCase().indexOf('MAC') >= 0
+}
