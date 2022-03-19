ec272f6a439aee24e073fcf35dee2997cd9ce720
kanno <812137533@qq.com>
2022-03-19 22:53:06 +0800

    perf: pickContextProps logic

--- packages/vue/src/utils/create/pick-context.ts
+++ packages/vue/src/utils/create/pick-context.ts
@@ -1,18 +1,15 @@
-import { assign, hasOwn } from '../format/object'
+import { assign } from '../format/object'
 
 export const pickContextProps = <T extends Record<string, any>, K extends Record<string, any> | null>(
   draft: T,
   parent: K,
   invert = false
 ): T => {
-  if (!parent) return draft
+  if (!parent || invert) return draft
   const source = parent.props || parent
-  const slice = Object.keys(source)
-  const len = slice.length
-  if (!len) return draft
+  const slice = Object.keys(draft)
   return slice.reduce((acc, cur) => {
-    if (!hasOwn(draft, cur)) return acc
-    const nil = !source[cur] || invert
+    const nil = !source[cur]
     return assign(acc, { [cur]: nil ? draft[cur] : source[cur] })
   }, {} as Pick<T, any>)
 }
