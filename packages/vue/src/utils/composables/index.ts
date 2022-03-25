fc5c8c4b3e7f3eda3b3d43b6d59b488deb1630fb
kanno <812137533@qq.com>
2022-03-25 18:22:45 +0800

    feat: remove use-real-shape

--- packages/vue/src/utils/composables/index.ts
+++ packages/vue/src/utils/composables/index.ts
@@ -1,6 +1,5 @@
 export * from './use-expose'
 export * from './use-portal'
-export * from './use-real-shape'
 export * from './use-resize'
 export * from './use-route'
 export * from './use-draggable'
