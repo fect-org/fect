f65afadf542ab0501e0a19feed54373486f93f13
kanno <812137533@qq.com>
2022-03-27 19:40:57 +0800

    fix(form-item): removed component can't clear apollo

---
+++ packages/vue/src/utils/composables/use-mount.ts
@@ -0,0 +1,38 @@
+import { onMounted, onBeforeUnmount } from 'vue'
+import { isArray, len } from '../format/arrary'
+
+type MountedFn = () => void
+
+export const useMounted = (fns: MountedFn[]) => {
+  if (!isArray(fns)) {
+    console.log('[Fect] useMounted must entry array')
+    return
+  }
+
+  const filterFunc = (fns: MountedFn[]) => {
+    const single = len(fns) === 1
+    if (single)
+      return {
+        start: fns[0],
+        end: fns[0]
+      }
+    const [, ...ends] = fns
+    return {
+      start: fns[0],
+      end: ends
+    }
+  }
+
+  const { start, end } = filterFunc(fns)
+
+  onMounted(start)
+
+  onBeforeUnmount(() => {
+    if (isArray(end)) {
+      end.forEach((_) => _.apply(null))
+      return
+    }
+    end()
+    return
+  })
+}
