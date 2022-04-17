9dd1978d69fff6d668258bd17bff44a588c7f081
kanno <812137533@qq.com>
2022-04-17 23:00:47 +0800

    feat: add unitCovert

--- packages/vue/src/utils/format/number.ts
+++ packages/vue/src/utils/format/number.ts
@@ -1,3 +1,12 @@
-export const isNumber = (val: any) => (Number.isNaN(Number(val)) ? false : true)
+/**
+ * we should convert val as Number type at first.
+ * so we can get the bsic type
+ * In fact, we treat nan as non-number type.
+ * After version 1.4.1
+ */
+export const isNumber = (val: unknown): val is number => {
+  val = Number(val)
+  return !Number.isNaN(val)
+}
 
 export const numberParser = (val: string) => Number.parseFloat(val)
