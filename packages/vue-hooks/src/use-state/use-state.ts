bd9a5effde4fa8f49cc92ce9d6f688b54575731a
柒宇 <48339849+QiYuOr2@users.noreply.github.com>
2022-04-27 18:16:17 +0800

    fix(vue-hooks): fix use-state type (#224)

--- packages/vue-hooks/src/use-state/use-state.ts
+++ packages/vue-hooks/src/use-state/use-state.ts
@@ -1,5 +1,5 @@
 import { ref, readonly } from 'vue'
-import type { UnwrapRef, Ref } from 'vue'
+import type { UnwrapRef, Ref, DeepReadonly } from 'vue'
 
 export type SetStateAction<S> = S | ((prevState: S) => S)
 
@@ -11,7 +11,7 @@ const useState = <T>(initial?: T) => {
     const draft = typeof next === 'function' ? (next as (prevState: T) => T)(state.value as T) : next
     state.value = draft as UnwrapRef<T>
   }
-  return [readonly(state), dispatch] as [Ref<T>, Dispatch<T>]
+  return [readonly(state), dispatch] as [DeepReadonly<Ref<T>>, Dispatch<T>]
 }
 
 export { useState }
