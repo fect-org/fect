22869c2182c6624deb46235e89a2d09e9aa95c93
qiyuor2 <48339849+QiYuOr2@users.noreply.github.com>
2022-05-04 21:04:21 +0800

    fix(switch): switch cannot watch the change of value

--- packages/vue/src/switch/switch.tsx
+++ packages/vue/src/switch/switch.tsx
@@ -1,6 +1,6 @@
 import { computed, PropType, defineComponent, watchEffect } from 'vue'
 import { useState } from '@fect-ui/vue-hooks'
-import { createName, UnknowProp, createBem, omit } from '../utils'
+import { createName, UnknowProp, createBem, omit, isUndefined } from '../utils'
 import { useFormStateContext, pickFormStateProps } from '../form/form-context'
 import type { NormalSizes } from '../utils'
 import type { SwitchEvent } from './interface'
@@ -47,7 +47,7 @@ export default defineComponent({
     const isChecked = () => currentValue.value === props.checkedValue
 
     watchEffect(() => {
-      setCurrentValue(props.value)
+      setCurrentValue(props.value ? props.value : props.modelValue)
     })
 
     const updateSwitchValue = (val: unknown) => {
