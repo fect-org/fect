e04ab639e97eb32318c38c3968a8922e322ccb2a
kanno <812137533@qq.com>
2022-05-26 23:27:05 +0800

    feat(utils): clean code

--- packages/vue/src/utils/format/interface.ts
+++ packages/vue/src/utils/format/interface.ts
@@ -1,7 +1,3 @@
-export type IntersectionToObj<T> = {
-  [P in keyof T]: T[P]
-}
-
 export type RecordPartial<T extends keyof any, K> = {
   [P in T]?: K
 }
--- packages/vue/src/utils/format/object.ts
+++ packages/vue/src/utils/format/object.ts
@@ -1,15 +1,10 @@
-import type { IntersectionToObj } from './interface'
-
 export const omit = <T, K extends keyof T>(source: T, picks: K[]) =>
   (Object.keys(source) as K[]).reduce(
     (acc, cur) => (picks.includes(cur) ? acc : Object.assign(acc, { [cur]: source[cur] })),
     {} as Omit<T, K>
   )
 
-export const assign = <T extends Record<string | number | symbol, any>, K>(
-  source: T,
-  ...rest: K[]
-): IntersectionToObj<Omit<T, keyof K> & K> => Object.assign(source, ...rest)
+export const { assign } = Object
 
 export const hasOwn = <T>(source: T, key: string) => Object.hasOwnProperty.call(source, key)
 
