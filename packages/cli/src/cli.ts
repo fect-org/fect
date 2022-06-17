950c95bb5ca2665748151bfcf6c730f51ce00e8f
kanno <812137533@qq.com>
2022-06-17 15:27:41 +0800

    feat: remove lint & test command

--- packages/cli/src/cli.ts
+++ packages/cli/src/cli.ts
@@ -1,6 +1,4 @@
 import { Command } from 'commander'
-import { jest } from './commands'
-import { format } from './node/lint'
 import { build } from './node/build'
 import { dev } from './node/dev'
 import { compile } from './node/compile'
@@ -11,13 +9,6 @@ program.command('compile').description('Compile Component code').action(compile)
 
 program.command('dev').description('Preview site on development environment').action(dev)
 
-program
-  .command('test')
-  .option('-u , --updateSnapshot', 'update jest Snapshot')
-  .description('run unit tests with jest')
-  .action(jest)
-
-program.command('lint').description('Lint code style').action(format)
 program.command('build').description('Build site ').action(build)
 
 program.parse()
--- packages/vue/jest.config.js
+++ packages/vue/jest.config.js
@@ -1,4 +1,11 @@
 module.exports = {
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: ['js', 'jsx', 'vue', 'ts', 'tsx'],
+  transform: {
+    '^.+\\.vue$': 'vue-jest',
+    '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
+    '\\.(js|jsx|ts|tsx)$': 'babel-jest'
+  },
   testPathIgnorePatterns: ['/node_modules/', '/dist/', '/lib/', '/es/'],
   collectCoverageFrom: [
     'src/**/*.{js,jsx,ts,tsx,vue}',
@@ -11,6 +18,11 @@ module.exports = {
     '!src/affix/**'
   ],
   collectCoverage: true,
-  preset: '@fect-ui/cli/lib/config',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  snapshotSerializers: ['jest-serializer-vue'],
+  testMatch: ['**/tests/unit/**/*.spec.[jt]s?(x)', '**/__tests__/*.[jt]s?(x)'],
+  testURL: 'http://localhost/',
   testTimeout: 20000
 }
