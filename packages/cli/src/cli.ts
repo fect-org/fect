a5b8b0a29db3689a307692fc8516505deec98b45
kanno <812137533@qq.com>
2022-06-17 15:37:42 +0800

    feat: remove command from cli

--- packages/cli/src/cli.ts
+++ packages/cli/src/cli.ts
@@ -1,14 +1,8 @@
 import { Command } from 'commander'
-import { build } from './node/build'
-import { dev } from './node/dev'
 import { compile } from './node/compile'
 
 const program = new Command()
 
 program.command('compile').description('Compile Component code').action(compile)
-
-program.command('dev').description('Preview site on development environment').action(dev)
-
-program.command('build').description('Build site ').action(build)
 
 program.parse()
--- packages/cli/src/config/vite.config.ts
+++ packages/cli/src/config/vite.config.ts
@@ -1,37 +1,5 @@
-import { merge } from 'lodash'
 import { InlineConfig } from 'vite'
-import Vue from '@vitejs/plugin-vue'
-import Jsx from '@vitejs/plugin-vue-jsx'
-import { CWD, VITE_RESOLVE_EXTENSIONS, UMD_PATH } from '../shared/constant'
-import { resolveConfig } from '../node/config'
-
-export const useDevConfig = async (): Promise<InlineConfig> => {
-  const { userConfig } = await resolveConfig()
-  const { port, plugins, viteConfigure } = userConfig
-
-  const presetConfig = {
-    root: CWD,
-    resolve: {
-      extensions: VITE_RESOLVE_EXTENSIONS,
-      alias: {}
-    },
-    server: {
-      port
-    },
-    plugins: [Vue({ include: [/\.vue$/, /\.md$/] }), Jsx(), ...plugins]
-  }
-
-  return merge(presetConfig, viteConfigure)
-}
-
-export const useBuildConfig = async (): Promise<InlineConfig> => {
-  const devConf = await useDevConfig()
-  return {
-    base: '/',
-    mode: 'production',
-    ...devConf
-  }
-}
+import { UMD_PATH } from '../shared/constant'
 
 export const useUMDconfig = (input, name, mini = false): InlineConfig => {
   return {
