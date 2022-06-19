418d186819735c392a353da32aeedda3a9419fd3
kanno <812137533@qq.com>
2022-06-19 13:22:56 +0800

    feat(internal): compile analyze style deps

--- internal/index.ts
+++ internal/index.ts
@@ -1,9 +1,9 @@
 export * from './bundle'
 export * from './process'
 
-export { jsx } from './plugins/jsx'
-
 export { analyze } from './plugins/analyze'
+
+export { css } from './plugins/css'
 
 export * from './helpers/gen'
 export * from './helpers/format'
--- internal/shared.ts
+++ internal/shared.ts
@@ -5,3 +5,8 @@ export const isScript = (suffix: string) => /\.(js|jsx|ts|tsx)/g.test(suffix)
 export const isStyle = (file: string) => /\.(css|less)$/.test(file)
 
 export const isJsx = (fileName: string) => /\.(jsx|tsx)/g.test(fileName)
+
+export const normalizePath = (path: string) => path.replace(/\\/g, '/')
+
+export const includes = <T extends unknown[], K extends unknown[]>(source: T, tar: K) =>
+  tar.every((v) => source.includes(v))
