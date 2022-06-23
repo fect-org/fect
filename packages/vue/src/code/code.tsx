9fc923bb7b8bbf81ce5ad2f0c0f8dfc3dcde9951
kanno <812137533@qq.com>
2022-06-23 16:28:33 +0800

    feat(code): support name and classic props

--- packages/vue/src/code/code.tsx
+++ packages/vue/src/code/code.tsx
@@ -1,33 +1,45 @@
-import { defineComponent } from 'vue'
-import { createName } from '../utils'
+import { computed, defineComponent } from 'vue'
+import { createName, addColorAlpha, createBem, isBrowser } from '../utils'
+import type { ComputedRef } from 'vue'
+import type { CSSProperties } from '../utils'
 
 import './index.less'
 
 const name = createName('Code')
 
+const bem = createBem('fect-code')
+
 export default defineComponent({
   name,
+  inheritAttrs: false,
   props: {
-    block: Boolean
+    block: Boolean,
+    name: String,
+    classic: Boolean
   },
-  setup(props, { slots }) {
-    const isBlock = () => {
-      return <code>{slots.default?.()}</code>
+  setup(props, { slots, attrs }) {
+    const withDefaultColor = () => {
+      if (!isBrowser) return ''
+      return window.getComputedStyle(document.documentElement).getPropertyValue('--accents-1')
     }
 
-    const normal = () => {
-      return (
-        <pre>
-          <code>{slots.default?.()}</code>
-        </pre>
-      )
-    }
+    const setStyle: ComputedRef<CSSProperties> = computed(() => {
+      const { classic } = props
+      return {
+        backgroundColor: classic ? 'var(--primary-background)' : addColorAlpha(withDefaultColor(), 0.75),
+        borderColor: classic ? 'var(--accents-2)' : 'var(--accents-1)'
+      }
+    })
 
     return () => {
-      if (props.block) {
-        return normal()
-      }
-      return isBlock()
+      if (!props.block) return <code {...attrs}>{slots.default?.()}</code>
+      const { name } = props
+      return (
+        <div class={bem('pre')} style={setStyle.value}>
+          {name && <header class={bem('name')}>{name}</header>}
+          <pre {...attrs}>{slots.default?.()}</pre>
+        </div>
+      )
     }
   }
 })
