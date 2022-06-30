01e4a807cdfe1246a94ce4a779ef73ea75b04349
kanno <812137533@qq.com>
2022-06-30 11:04:14 +0800

    feat: add backdrop component

---
+++ packages/vue/src/utils/create/with-classname.ts
@@ -0,0 +1,6 @@
+import { len } from '../format'
+
+export const withClassName = (...calssNames: string[]) => {
+  if (!len(calssNames)) return ''
+  return calssNames.reduce((acc, cur) => (acc += ` ${cur}`), '')
+}
