6273e384ca23c74d5a193fb70917e03fa7cb40f6
kanno <812137533@qq.com>
2022-07-04 17:08:59 +0800

    fix(code): use css replace past

--- packages/vue/src/code/code.tsx
+++ packages/vue/src/code/code.tsx
@@ -1,9 +1,5 @@
-import { computed, defineComponent, watch } from 'vue'
-import { useState } from '@fect-ui/vue-hooks'
-import { createName, addColorAlpha, createBem, isBrowser } from '../utils'
-import { useTheme } from '../composables'
-import type { ComputedRef } from 'vue'
-import type { CSSProperties } from '../utils'
+import { defineComponent } from 'vue'
+import { createName, createBem } from '../utils'
 
 import './index.less'
 
@@ -20,32 +16,11 @@ export default defineComponent({
     classic: Boolean
   },
   setup(props, { slots, attrs }) {
-    const { theme } = useTheme({ withModify: false })
-    const [hex, setHex] = useState('')
-
-    watch(
-      () => theme.value,
-      () => {
-        if (!isBrowser()) return
-        const hex = window.getComputedStyle(document.documentElement).getPropertyValue('--accents-1')
-        setHex(hex)
-      },
-      { immediate: true }
-    )
-
-    const setStyle: ComputedRef<CSSProperties> = computed(() => {
-      const { classic } = props
-      return {
-        backgroundColor: classic ? 'var(--primary-background)' : addColorAlpha(hex.value, 0.75),
-        borderColor: classic ? 'var(--accents-2)' : 'var(--accents-1)'
-      }
-    })
-
     return () => {
       if (!props.block) return <code {...attrs}>{slots.default?.()}</code>
-      const { name } = props
+      const { name, classic } = props
       return (
-        <div class={bem('pre')} style={setStyle.value}>
+        <div class={bem('pre', { classic })}>
           {name && <header class={bem('name')}>{name}</header>}
           <pre {...attrs}>{slots.default?.()}</pre>
         </div>
