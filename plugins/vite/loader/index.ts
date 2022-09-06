0a08909911d1d2b95ed1e9c38c54c8fca7192e06
kanno <812137533@qq.com>
2022-09-06 14:32:20 +0800

    feat(virtual): join collection

--- plugins/vite/loader/index.ts
+++ plugins/vite/loader/index.ts
@@ -11,25 +11,72 @@ import fg from 'fast-glob'
 import fs from 'fs/promises'
 import { composeSfcBlocks } from 'vite-plugin-md'
 
-import type { Plugin } from 'vite'
-
-const defaultWd = process.cwd()
-
-const websiteWd = path.join(defaultWd, 'docs', 'layout', 'common')
+import type { ModuleNode, Plugin, ViteDevServer } from 'vite'
 
 const slash = (path: string) => path.replace(/\\/g, '/')
+
+const defaultWd = slash(process.cwd())
 
 const virtualModule = `virtual:router`
 
 const virtualId = '/@virtual:router/modules'
 
-const initialize = () => {
-  return {}
-}
+/**
+ * vite's watcher is use chokidar.
+ * See the api document:  https://github.com/paulmillr/chokidar#api
+ */
+const ensureServerRuning = (server: ViteDevServer, markdowns: Map<string, StaticMarkdonMeta>) => {
+  const { watcher } = server
 
-// const ensureServer = () => {
-//   //
-// }
+  // Here is a footgun. we just think the suffix *.md is allowed
+  const isTargetFile = (file: string) => path.extname(file).split('.')[1] === 'md'
+
+  const invorkFSWatcher = (file, callback: (file: string, reloadPage: () => void) => void) => {
+    file = slash(file)
+    if (!isTargetFile(file)) return
+    // https://vitejs.dev/guide/why.html#slow-updates
+    const reloadPage = () => {
+      if (!server) return
+      const { ws, moduleGraph } = server
+      const mods = moduleGraph.getModulesByFile(virtualModule)
+      if (mods) {
+        const seen = new Set<ModuleNode>()
+        mods.forEach((mod) => moduleGraph.invalidateModule(mod, seen))
+      }
+      ws.send({ type: 'full-reload' })
+    }
+    callback(file, reloadPage)
+  }
+
+  watcher.on('change', (file) =>
+    invorkFSWatcher(file, async (realFile, reloadPage) => {
+      if (markdowns.has(realFile)) {
+        const latest = await loadModule(realFile)
+        joinMapStruct<StaticMarkdonMeta>([latest], (data, key) => markdowns.set(key, data))
+        reloadPage()
+      }
+    })
+  )
+
+  watcher.on('unlink', (file) =>
+    invorkFSWatcher(file, (realFile, reloadPage) => {
+      if (markdowns.has(realFile)) {
+        markdowns.delete(file)
+        reloadPage()
+      }
+    })
+  )
+
+  watcher.on('add', (file) =>
+    invorkFSWatcher(file, async (realFile, reloadPage) => {
+      const latest = await loadModule(realFile)
+      joinMapStruct<StaticMarkdonMeta>([latest], (data, key) => {
+        if (!markdowns.has(key)) markdowns.set(key, data)
+      })
+      reloadPage()
+    })
+  )
+}
 
 const parserRequest = (id: string) => {
   const [moduleId, raw] = id.split('?', 2)
@@ -49,44 +96,92 @@ const loadGlob = (entry: string, ext = '.md') => {
   })
 }
 
+const loadModule = async (file: string) => {
+  //
+  const staticRaw = await fs.readFile(file, 'utf8')
+  const {
+    frontmatter: { title, index, name, group }
+  } = await composeSfcBlocks(file, staticRaw)
+  return {
+    title,
+    index,
+    name: name?.toLocaleLowerCase(),
+    group,
+    file
+  }
+}
+
 const loadStaticMarkdonModuleImpl = async () => {
-  const originalModulePath = slash(path.join(defaultWd, 'docs'))
   const zh = await loadGlob(slash(path.join(defaultWd, 'docs', 'zh-cn')))
   const en = await loadGlob(slash(path.join(defaultWd, 'docs', 'en-us')))
-  const serialization = async (files: string[]) => {
-    return Promise.all(
-      files.map(async (file) => {
-        const staticRaw = await fs.readFile(file, 'utf8')
-        const {
-          frontmatter: { title, index, name, group }
-        } = await composeSfcBlocks(file, staticRaw)
-        const dirName = path.basename(slash(path.dirname(file)).replace(originalModulePath, ''))
-        // console.log(slash(path.relative(websiteWd, file)))
-        // https://github1s.com/vitejs/vite/blob/HEAD/packages/vite/src/node/plugins/importMetaGlob.ts
-        return {
-          title,
-          index,
-          name: name?.toLocaleLowerCase(),
-          group,
-          dirName
-          //   module: () => '123'
-        }
-      })
-    )
-  }
+  // https://github1s.com/vitejs/vite/blob/HEAD/packages/vite/src/node/plugins/importMetaGlob.ts
+  const serialization = async (files: string[]) => Promise.all(files.map(loadModule))
   const asyncLoad = () => Promise.all([serialization(zh), serialization(en)])
   return asyncLoad()
 }
 
+const resolver = (markdowns: Map<string, StaticMarkdonMeta>) => {
+  const store: {
+    'en-us': any[]
+    'zh-cn': any[]
+  } = {
+    'en-us': [],
+    'zh-cn': []
+  }
+  const imports: string[] = []
+  const realDirName = (file) => path.dirname(file).split('/').pop()
+  const realImport = (variable: string, file: string) => {
+    const source = file.replace(slash(path.join(defaultWd, 'docs')), '')
+    return `const ${variable} = () => import("${source}")`
+  }
+  const componentRE = /"(?:module|element)":("(.*?)")/g
+  markdowns.forEach(({ group, index, title, name }, key) => {
+    if (key.includes('en-us')) {
+      const moduleName = `${name}_en_us`
+      imports.push(realImport(moduleName, key))
+      store['en-us'].push({ group, index, dirName: realDirName(key), name, title, module: moduleName })
+    }
+    if (key.includes('zh-cn')) {
+      const moduleName = `${name}_zh_cn`
+      imports.push(realImport(moduleName, key))
+      store['zh-cn'].push({ group, index, dirName: realDirName(key), name, title, module: moduleName })
+    }
+  })
+  const final = Object.values(store)
+  const c = JSON.stringify(final).replace(componentRE, (_, $1) => {
+    $1 = $1.replace(/\"/g, '')
+    return _
+  })
+  console.log(c)
+  const code = `${imports.join(';\n')};const routes =${JSON.stringify(final)}; \n\nexport default routes;`
+  return code
+}
+
+type StaticMarkdonModule = Awaited<ReturnType<typeof loadStaticMarkdonModuleImpl>>[number]
+
+type StaticMarkdonMeta = Omit<StaticMarkdonModule[number], 'file'>
+
+const joinMapStruct = <K>(raw: StaticMarkdonModule, invork: (data: K, key: string) => void) => {
+  raw.forEach((item) => {
+    const { file, ...rest } = item
+    invork(rest as K, file)
+  })
+}
+
 export const loadStaticMarkdonModule = (): Plugin => {
   //
-  let cc: any = null
+  const markdowns = new Map<string, StaticMarkdonMeta>()
   return {
     enforce: 'pre',
     name: 'virtual:loader',
     async buildStart() {
       const originalModule = await loadStaticMarkdonModuleImpl()
-      cc = originalModule
+      joinMapStruct<StaticMarkdonMeta>(originalModule.flat(), (data, key) => {
+        if (!markdowns.has(key)) markdowns.set(key, data)
+      })
+    },
+    configureServer(server) {
+      ensureServerRuning(server, markdowns)
     },
     resolveId(id) {
       if (virtualModule === id) return `${virtualId}?id=${id}`
@@ -95,7 +190,7 @@ export const loadStaticMarkdonModule = (): Plugin => {
     async load(id) {
       const { moduleId, pageId } = parserRequest(id)
       if (moduleId === virtualId && pageId) {
-        return `export default ${JSON.stringify(cc)}`
+        return resolver(markdowns)
       }
       return null
     }
