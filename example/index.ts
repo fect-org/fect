996a3692c4a3e663a4a4e1eed95cfb48dc9a372b
kanno <812137533@qq.com>
2022-09-06 16:00:19 +0800

    chore: mege example bundle

--- example/index.ts
+++ example/index.ts
@@ -4,14 +4,10 @@ export default {
   install: (vue: App) => {
     const exampels = import.meta.glob<{
       default: Component<{ name: string }>
-    }>('./**/*.vue')
-    ;(async () => {
-      await Promise.all(
-        Object.values(exampels).map(async (module) => {
-          const { default: component } = await module()
-          vue.component(component.name, component)
-        })
-      )
-    })()
+    }>('./**/*.vue', { eager: true })
+    Object.values(exampels).map((module) => {
+      const { default: component } = module
+      vue.component(component.name, component)
+    })
   }
 }
