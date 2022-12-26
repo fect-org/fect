07f08ba7b028e92b04bd93950f716c16ef5b0103
kanno <812137533@qq.com>
2022-12-26 15:09:09 +0800

    chore: remove style9

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
 import Jsx from '@vitejs/plugin-vue-jsx'
 import Md from 'vite-plugin-md'
-import Style9 from 'style9/vite'
 import { cdn } from 'vite-plugin-cdn2'
 import Prism from 'prismjs'
 import path from 'path'
@@ -23,12 +22,6 @@ export default defineConfig({
     Vue({
       include: [/\.vue$/, /\.md$/]
     }),
-    {
-      ...Style9({
-        include: [/\.[jt]sx?$/, /\.vue$/, /\.md$/]
-      }),
-      enforce: 'post'
-    },
     /**
      * From vite-plugin-md we can define the custom frontmatter.
      * docs: https://github.com/yankeeinlondon/meta-builder
